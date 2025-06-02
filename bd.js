782b16561c7a9f801a4d3e6fb62fc4a5b3522b94
Jhonoaru <113635294+Jhonoaru@users.noreply.github.com>
2025-06-02 18:35:37 -0300

    Update bd.js

--- bd.js
+++ bd.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); // <-- carrega o .env
 const { logarErro } = require('./logger');
 
 async function conectarBanco() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI); // <-- usa a variável do .env
     console.log('✅ Conectado ao MongoDB');
   } catch (erro) {
     logarErro('Erro ao conectar ao MongoDB: ' + erro.message);
